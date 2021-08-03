b90dd9d49fa00c7e9d2e81d09a3cf72320cd4470
Alex <alexhinds506@gmail.com>
2021-08-03 18:33:16 -0400

    added the current date to application

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
 // Function to save scheduled events to local storage
+var textArea = $('textarea');
+var saveButton = $('.saveBtn');
+
+var textAreObj = {
+    text: textEl,
+    time: timeEl
+};
+
+saveButton.click(function () {
+    var textEl = $(this).siblings(textArea).val();
+    // dynamically add class or in html to textEl variable
+    var timeEl = $(this).siblings()
+    // add an attritube to timeEl (id)
+
+});
+
 // var saveEvents = function() {localStorage.setItem("variable", JSON.stringify(variable))};
 
 // Function to display the current date using Moment 
-// var today = moment();
-// console.log("Today's date is:" + today.format('YYYY-MM-DD'));
+var today = moment().format('dddd, ' + 'MMMM Do YYYY');
+
+var currentDay = $('#currentDay');
+currentDay.text(today);
