37992096fa7381cc41323befb62788c9d21d957c
Alex <alexhinds506@gmail.com>
2021-08-03 15:08:04 -0400

    commented out the start of the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-var today = moment();
-console.log(
-    "Today's date is: " + today.format('YYYY-MM-DD')
-);
+// Function to save scheduled events to local storage
+// var saveEvents = function() {localStorage.setItem("variable", JSON.stringify(variable))};
+
+// Function to display the current date using Moment 
+// var today = moment();
+// console.log("Today's date is:" + today.format('YYYY-MM-DD'));
