700f4c664b44eb6db3cdb7ab515bbbbfaa6cadc5
Alex <alexhinds506@gmail.com>
2021-08-07 11:11:43 -0400

    add html and js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,52 @@
-// Function to save scheduled events to local storage
-var textArea = $('textarea');
-var saveButton = $('.saveBtn');
+$(document).ready(function () {
 
-var textAreObj = {
-    text: textEl,
-    time: timeEl
-};
 
-saveButton.click(function () {
-    var textEl = $(this).siblings(textArea).val();
-    // dynamically add class or in html to textEl variable
-    var timeEl = $(this).siblings()
-    // add an attritube to timeEl (id)
+    // Function to save scheduled events to local storage
+    // var textArea = $('.textarea');
+    // var saveButton = $('.saveBtn');
+
+    // saveButton.click(function () {
+    //     var textEl = $(this).siblings(textArea).val();
+    //     // dynamically add class or in html to textEl variable
+    //     var timeEl = $(this).siblings('#time').val();
+    //     // add an attritube to timeEl (id)
+
+    //     localStorage.setItem(textEl, timeEl);
+
+
+    // });
+
+    $('.saveBtn').on('click', function () {
+        // get nearby values
+        var textEL = $(this)
+            .siblings('.textarea')
+            .val();
+
+        // var timeEl = $(this).siblings('.time').val();
+        var timeEl = $(this)
+            .parent()
+            .attr('.id');
+
+        // save in localStorage
+        localStorage.setItem(timeEl, textEL);
+    });
+
+    $('#hour-10 .textarea').val(localStorage.getItem('hour-10'));
+
+    // var textAreObj = {
+    //     text: textEl,
+    //     time: timeEl
+    // };
+
+
+    // var saveEvents = function() {localStorage.setItem("variable", JSON.stringify(variable))};
+
+    // Function to display the current date using Moment 
+    // var today = moment().format('dddd, ' + 'MMMM Do YYYY');
+
+    // var currentDay = $('#currentDay');
+    // currentDay.text(today);
+
+    $('#currentDay').text(moment().format('dddd, MMMM Do'));
 
 });
-
-// var saveEvents = function() {localStorage.setItem("variable", JSON.stringify(variable))};
-
-// Function to display the current date using Moment 
-var today = moment().format('dddd, ' + 'MMMM Do YYYY');
-
-var currentDay = $('#currentDay');
-currentDay.text(today);
