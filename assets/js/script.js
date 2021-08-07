cd0a1ba054db820acf8be435b851d87593e467c8
Alex <alexhinds506@gmail.com>
2021-08-07 11:39:34 -0400

    added ability to save to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,20 @@
 $(document).ready(function () {
-
-
-    // Function to save scheduled events to local storage
-    // var textArea = $('.textarea');
-    // var saveButton = $('.saveBtn');
-
-    // saveButton.click(function () {
-    //     var textEl = $(this).siblings(textArea).val();
-    //     // dynamically add class or in html to textEl variable
-    //     var timeEl = $(this).siblings('#time').val();
-    //     // add an attritube to timeEl (id)
-
-    //     localStorage.setItem(textEl, timeEl);
-
-
-    // });
-
     $('.saveBtn').on('click', function () {
-        // get nearby values
         var textEL = $(this)
             .siblings('.textarea')
             .val();
-
-        // var timeEl = $(this).siblings('.time').val();
         var timeEl = $(this)
             .parent()
-            .attr('.id');
+            .attr('id');
 
         // save in localStorage
         localStorage.setItem(timeEl, textEL);
     });
 
+    // Save's data to localStorage
     $('#hour-10 .textarea').val(localStorage.getItem('hour-10'));
 
-    // var textAreObj = {
-    //     text: textEl,
-    //     time: timeEl
-    // };
-
-
-    // var saveEvents = function() {localStorage.setItem("variable", JSON.stringify(variable))};
-
-    // Function to display the current date using Moment 
-    // var today = moment().format('dddd, ' + 'MMMM Do YYYY');
-
-    // var currentDay = $('#currentDay');
-    // currentDay.text(today);
-
+    // Displays current day using moment 
     $('#currentDay').text(moment().format('dddd, MMMM Do'));
-
 });
+
